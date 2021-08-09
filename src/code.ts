41dc6af0e9ae9de45f53007485da612751132192
RaeWinTan <raewintan@gmail.com>
2021-08-09 14:56:57 +0800

    first debug version

--- src/code.ts
+++ src/code.ts
@@ -5,7 +5,7 @@ import { concat} from "rxjs";
 import { setUp$, ui$ } from "./setup";
 import {shots$} from "./shots";
 import { switchMap } from "rxjs/operators";
-
+//debuging purpose, where computer and player shots can be seen
 const game$ = ui$.pipe(
   switchMap((initial:Boards)=>concat(setUp$(initial), shots$(initial)))
 );
